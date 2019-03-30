fbce38f5870a12c55e6c6f575658393f0915a9b6
nawal.zakarya@gmail.com <nawal.zakarya@gmail.com>
2019-03-30 04:14:04 +0100

    Finissions design home page

--- public/index.php
+++ public/index.php
@@ -1,3 +1,26 @@
+<?php
+
+require '../src/connec.php';
+$pdo = new PDO(DSN, USER, PASS);
+
+/** DATA RETRIEVAL */
+
+$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+// recovers categories table //
+$queryCategory = "SELECT * FROM category";
+$statementCategory = $pdo->query($queryCategory);
+$categories = $statementCategory->fetchAll(PDO::FETCH_ASSOC);
+// recovers evaluations table //
+$queryEval = "SELECT * FROM eval";
+$statementEval = $pdo->query($queryEval);
+$evaluations = $statementEval->fetchAll(PDO::FETCH_ASSOC);
+// recovers ideas table //
+$queryIdea = "SELECT * FROM idea";
+$statementIdea = $pdo->query($queryIdea);
+$ideas = $statementIdea->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -9,20 +32,17 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://bootswatch.com/4/sketchy/bootstrap.min.css">
+    <link rel="stylesheet" href="https://bootswatch.com/4/materia/bootstrap.min.css">
     <link rel="stylesheet" href="assets/style.css"/>
     <link rel="stylesheet" href="assets/cards.css"/>
 
     <title>Just box it</title>
 </head>
-<body>
+<body class="m-0">
 
 <?php require 'header.php' ?>
 
-
-
 <?php require 'footer.php'?>
-
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
