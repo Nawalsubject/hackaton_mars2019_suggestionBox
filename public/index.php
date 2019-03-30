3d0d41caff109067077b62975d490ec044e54720
Damien Rambourg <damien.rambourg@gmail.com>
2019-03-30 04:29:37 +0100

    Section top article initial

--- public/index.php
+++ public/index.php
@@ -18,12 +18,47 @@
 
 <h1>Hello, world!</h1>
 
+
+<section class="carousel slide" class="my-3" data-ride="carousel" id="postsCarousel">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center mb-4">
+                <a class="btn btn-outline-secondary prev" href="" title="go back"><i class="fa fa-lg fa-chevron-left"></i></a>
+                <a class="btn btn-outline-secondary next" href="" title="more"><i class="fa fa-lg fa-chevron-right"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="container p-t-2 m-t-2 carousel-inner">
+        <div class="row row-equal carousel-item active m-t-0">
+            <?php include '../src/cards.php' ?>
+            <?php include '../src/cards.php' ?>
+            <?php include '../src/cards.php' ?>
+
+        </div>
+        <div class="row row-equal carousel-item m-t-0">
+            <?php include '../src/cards.php' ?>
+            <?php include '../src/cards.php' ?>
+            <?php include '../src/cards.php' ?>
+        </div>
+    </div>
+</section>
+
+
 <?php require 'footer.php'?>
 
 <!-- Optional JavaScript -->
+
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script>
+    (function($){
+        "use strict";
+        $('.next').click(function(){ $('.carousel').carousel('next');return false; });
+        $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
+    })
+    (jQuery);
+</script>
 </body>
 </html>
