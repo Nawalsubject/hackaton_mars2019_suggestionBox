49e7cd236a4aede061da4a26c73daedacc37f20a
Vinny <paulvinny@gmail.com>
2019-03-30 06:37:39 +0100

    bug fixes on best idea

--- public/header.php
+++ public/header.php
@@ -1,6 +1,4 @@
 <?php
-
-
 ?>
 
 
@@ -15,36 +13,22 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="#">Acceuil<span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="formulaire.php">Nouvelle idée</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="index.php#bestID">Les meilleurs idées</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-
         </ul>
         <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+            <!-- <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button> -->
         </form>
     </div>
 </nav>
 
-<div class="jumbotron headerBotron jumbotron-fluid">
-    <div class="container justify-content-center">
-        <h1 class="display-4">Idée du jour</h1>
 
-        <?php require '../src/card_large.php'?>
-
-    </div>
-</div>
 
 </header>
--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 require '../src/connec.php';
 require '../src/functions.php';
+require '../src/counterIdea.php';
 $pdo = new PDO(DSN, USER, PASS);
 
 /** DATA RETRIEVAL */
@@ -30,8 +31,10 @@ $idea = $ideas[randomIdeas($ideas)];
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link rel="stylesheet" href="https://bootswatch.com/4/materia/bootstrap.min.css">
     <link rel="stylesheet" href="assets/style.css"/>
     <link rel="stylesheet" href="assets/cards.css"/>
@@ -41,23 +44,18 @@ $idea = $ideas[randomIdeas($ideas)];
 <body class="m-0">
 
 <?php require 'header.php' ?>
-<section>
-    <?php require '../src/card_large.php' ?>
-</section>
-
-
 
 <div class="jumbotron headerBotron jumbotron-fluid">
     <div class="container justify-content-center">
         <h1 class="text-warning text-center my-3">L'IDÉE DU JOUR </h1>
 
-        <?php require '../src/card_large.php'?>
+        <?php require '../src/card_large.php' ?>
 
     </div>
 </div>
 
 <section>
-    <h2 class="text-warning text-center my-3">LES MEILLEURES IDEES DU MOMENT ...</h2>
+    <h2 id="bestID" class="text-warning text-center my-3">LES MEILLEURES IDEES DU MOMENT ...</h2>
     <div class="container">
         <div class="row">
             <?php include '../src/cards.php' ?>
@@ -68,18 +66,30 @@ $idea = $ideas[randomIdeas($ideas)];
 </section>
 
 
-<?php require 'footer.php'?>
+<?php require 'footer.php' ?>
 <!-- Optional JavaScript -->
 
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 <script>
-    (function($){
+    (function ($) {
         "use strict";
-        $('.next').click(function(){ $('.carousel').carousel('next');return false; });
-        $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
+        $('.next').click(function () {
+            $('.carousel').carousel('next');
+            return false;
+        });
+        $('.prev').click(function () {
+            $('.carousel').carousel('prev');
+            return false;
+        });
     })
     (jQuery);
 </script>
