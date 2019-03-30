5056861e6c792fd4f1f1a2f6dcaba55c24009a92
nawal.zakarya@gmail.com <nawal.zakarya@gmail.com>
2019-03-30 02:57:52 +0100

    Derniere modifs de la card 'idée du jour' + ajout desgin

--- public/header.php
+++ public/header.php
@@ -32,36 +32,25 @@
 </nav>
 
 <div class="jumbotron headerBotron jumbotron-fluid">
-    <div class="container">
+    <div class="container justify-content-center">
         <h1 class="display-4">Idée du jour</h1>
-            <div class="card user-card">
-                <div class="card-block py-3 text-left">
-                    <h5 class="f-w-600">regis<!-- l'identité se trouvera ici --></h5>
-                </div>
+            <div class="card user-card pb-0">
                 <div class="card-header py-3 text-center">
                     <h4 class="col">Plus de bière</h4>
                 </div>
                 <div class="card-block">
                     <div class="">
                         <p class="m-t-15 text-muted text-justify">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                            Aenean commodo
-                            ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et ma
+                            Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et ma Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                            Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et ma - <i class="text-muted">Regis<!-- date de l'idée --></i>
+
                         </p>
 
                     </div>
                     <hr>
-                    <p class="text-muted m-t-15 mb-1"><!-- Barometre good / bad idea -->86%</p>
-                    <!-- activity-leval-blue // activity-leval-green // activity-leval-yellow // -->
-                    <ul class="list-unstyled activity-leval activity-leval-blue">
-                        <li class="active"></li>
-                        <li class="active"></li>
-                        <li class="active"></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
                     <!-- bg-c-blue // bg-c-green // bg-c-yellow // -->
-                    <div class="bg-c-blue counter-block m-t-10 p-15">
-                        <div class="row">
+
+                    <div class="row">
                             <div class="col-4">
                                 <i class="fas fa-heart-broken"></i>
                                 <p>1256</p> <!-- nombre de bad mother fucker -->
@@ -74,9 +63,7 @@
                                 <i class="fas fa-heart"></i>
                                 <p>189</p><!-- nombre de fuck yeah !  -->
                             </div>
-                        </div>
                     </div>
-                    <p class="mb-0 mt-1 pt-2 text-muted">hier<!-- date de l'idée --></p>
                 </div>
             </div>
     </div>
