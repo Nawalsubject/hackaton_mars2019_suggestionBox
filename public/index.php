82fc0c955e833441ca2612544118de445ca9c231
Vang Jeannette <vangjeannette@gmail.com>
2019-03-30 01:34:19 +0100

    ajout du fichier fomulaire.php avec style.css

--- public/index.php
+++ public/index.php
@@ -1,3 +1,4 @@
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -7,11 +8,15 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="assets/style.css"/>
     <title>Just box it</title>
 </head>
 <body>
-<h1>Hello, world!</h1>
+
+<h1></h1>
+
+
+
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
