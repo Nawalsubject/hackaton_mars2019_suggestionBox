1bc03de239d329f77e2b1379aea7424d199e3842
Vinny <paulvinny@gmail.com>
2019-03-30 01:05:24 +0100

    add color

--- src/cards.php
+++ src/cards.php
@@ -4,21 +4,23 @@
 
 <div class="col-md-4">
     <div class="card user-card">
-        <div class="card-header row">
-            <h5 class="col">Plus de bière</h5>
-            <p class=" col text-muted">"Idée évolutive"</p>
+        <div class="card-block py-3 text-left">
+            <h5 class="f-w-600">regis<!-- l'identité se trouvera ici --></h5>
+        </div>
+        <div class="card-header py-3 text-center">
+            <h4 class="col">Plus de bière</h4>
         </div>
         <div class="card-block">
             <div class="">
-                <p class="m-t-15 text-muted">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+                <p class="m-t-15 text-muted text-justify">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                    Aenean commodo
                     ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et ma
                 </p>
 
             </div>
-            <h6 class="f-w-600 m-t-25 m-b-10">regis<!-- l'identité se trouvera ici --></h6>
-            <p class="text-muted">hier<!-- date de l'idée --></p>
             <hr>
-            <p class="text-muted m-t-15"><!-- Barometre good / bad idea -->86%</p>
+            <p class="text-muted m-t-15 mb-1"><!-- Barometre good / bad idea -->86%</p>
+            <!-- activity-leval-blue // activity-leval-green // activity-leval-yellow // -->
             <ul class="list-unstyled activity-leval activity-leval-blue">
                 <li class="active"></li>
                 <li class="active"></li>
@@ -27,118 +29,23 @@
                 <li></li>
             </ul>
             <!-- bg-c-blue // bg-c-green // bg-c-yellow // -->
-            <div class="bg-c-blue counter-block m-t-10 p-20">
+            <div class="bg-c-blue counter-block m-t-10 p-15">
                 <div class="row">
                     <div class="col-4">
-                        <i class="fa fa-comment"></i>
+                        <i class="fas fa-heart-broken"></i>
                         <p>1256</p> <!-- nombre de bad mother fucker -->
                     </div>
                     <div class="col-4">
-                        <i class="fa fa-user"></i>
+                        <i class="fa fa-comment"></i>
                         <p>8562</p><!-- nombre de commentaires -->
                     </div>
                     <div class="col-4">
-                        <i class="fa fa-suitcase"></i>
+                        <i class="fas fa-heart"></i>
                         <p>189</p><!-- nombre de fuck yeah !  -->
                     </div>
                 </div>
             </div>
-            <!-- idée -->
-        </div>
-    </div>
-</div>
-
-
-<div class="col-md-4">
-    <div class="card user-card">
-        <div class="card-header row">
-            <h5 class="col">Plus de bière</h5>
-            <p class="col text-muted">"Idée neuve"</p>
-        </div>
-        <div class="card-block">
-
-            <div class="">
-                <p class="m-t-15 text-muted">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-                    ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et ma
-                </p>
-
-            </div>
-            <h6 class="f-w-600 m-t-25 m-b-10">regis<!-- l'identité se trouvera ici --></h6>
-            <p class="text-muted">hier<!-- date de l'idée --></p>
-            <hr>
-            <p class="text-muted m-t-15"><!-- Barometre good / bad idea -->86%</p>
-            <ul class="list-unstyled activity-leval activity-leval-green">
-                <li class="active"></li>
-                <li class="active"></li>
-                <li class="active"></li>
-                <li></li>
-                <li></li>
-            </ul>
-            <!-- bg-c-blue // bg-c-green // bg-c-yellow // -->
-            <div class="bg-c-green counter-block m-t-10 p-20">
-                <div class="row">
-                    <div class="col-4">
-                        <i class="fa fa-comment"></i>
-                        <p>1256</p> <!-- nombre de bad mother fucker -->
-                    </div>
-                    <div class="col-4">
-                        <i class="fa fa-user"></i>
-                        <p>8562</p><!-- nombre de commentaires -->
-                    </div>
-                    <div class="col-4">
-                        <i class="fa fa-suitcase"></i>
-                        <p>189</p><!-- nombre de fuck yeah !  -->
-                    </div>
-                </div>
-            </div>
-            <!-- idée -->
-        </div>
-    </div>
-</div>
-
-<div class="col-md-4">
-    <div class="card user-card">
-        <div class="card-header row">
-            <h5 class="col">Plus de bière</h5>
-            <p class=" col text-muted">"Idée insolite"</p>
-        </div>
-        <div class="card-block">
-
-            <div class="">
-                <p class="m-t-15 text-muted">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-                    ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et ma
-                </p>
-
-            </div>
-            <h6 class="f-w-600 m-t-25 m-b-10">regis<!-- l'identité se trouvera ici --></h6>
-            <p class="text-muted">hier<!-- date de l'idée --></p>
-            <hr>
-            <p class="text-muted m-t-15"><!-- Barometre good / bad idea -->86%</p>
-            <ul class="list-unstyled activity-leval activity-leval-yellow">
-                <li class="active"></li>
-                <li class="active"></li>
-                <li class="active"></li>
-                <li></li>
-                <li></li>
-
-            </ul>
-            <!-- bg-c-blue // bg-c-green // bg-c-yellow // -->
-            <div class="bg-c-yellow counter-block m-t-10 p-20">
-                <div class="row">
-                    <div class="col-4">
-                        <i class="fa fa-comment"></i>
-                        <p>1256</p> <!-- nombre de bad mother fucker -->
-                    </div>
-                    <div class="col-4">
-                        <i class="fa fa-user"></i>
-                        <p>8562</p><!-- nombre de commentaires -->
-                    </div>
-                    <div class="col-4">
-                        <i class="fa fa-suitcase"></i>
-                        <p>189</p><!-- nombre de fuck yeah !  -->
-                    </div>
-                </div>
-            </div>
+            <p class="mb-0 mt-1 pt-2 text-muted">hier<!-- date de l'idée --></p>
         </div>
     </div>
 </div>
