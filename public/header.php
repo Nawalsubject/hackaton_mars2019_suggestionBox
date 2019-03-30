826397b58d9c0005a8fcdcdd72d57463aadb82dc
nawal.zakarya@gmail.com <nawal.zakarya@gmail.com>
2019-03-30 01:16:57 +0100

    Ajout de card dans header

--- public/header.php
+++ public/header.php
@@ -1,3 +1,4 @@
+<header>
 <nav class="navbar sticky-top navbar-expand-lg py-2">
     <a class="navbar-brand" href="#">Just box it</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,13 +28,58 @@
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
         </form>
-
-        <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <h1 class="display-4">Fluid jumbotron</h1>
-                <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-            </div>
-        </div>
-
     </div>
 </nav>
+
+<div class="jumbotron headerBotron jumbotron-fluid">
+    <div class="container">
+        <h1 class="display-4">Idée du jour</h1>
+            <div class="card user-card">
+                <div class="card-block py-3 text-left">
+                    <h5 class="f-w-600">regis<!-- l'identité se trouvera ici --></h5>
+                </div>
+                <div class="card-header py-3 text-center">
+                    <h4 class="col">Plus de bière</h4>
+                </div>
+                <div class="card-block">
+                    <div class="">
+                        <p class="m-t-15 text-muted text-justify">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                            Aenean commodo
+                            ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et ma
+                        </p>
+
+                    </div>
+                    <hr>
+                    <p class="text-muted m-t-15 mb-1"><!-- Barometre good / bad idea -->86%</p>
+                    <!-- activity-leval-blue // activity-leval-green // activity-leval-yellow // -->
+                    <ul class="list-unstyled activity-leval activity-leval-blue">
+                        <li class="active"></li>
+                        <li class="active"></li>
+                        <li class="active"></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                    <!-- bg-c-blue // bg-c-green // bg-c-yellow // -->
+                    <div class="bg-c-blue counter-block m-t-10 p-15">
+                        <div class="row">
+                            <div class="col-4">
+                                <i class="fas fa-heart-broken"></i>
+                                <p>1256</p> <!-- nombre de bad mother fucker -->
+                            </div>
+                            <div class="col-4">
+                                <i class="fa fa-comment"></i>
+                                <p>8562</p><!-- nombre de commentaires -->
+                            </div>
+                            <div class="col-4">
+                                <i class="fas fa-heart"></i>
+                                <p>189</p><!-- nombre de fuck yeah !  -->
+                            </div>
+                        </div>
+                    </div>
+                    <p class="mb-0 mt-1 pt-2 text-muted">hier<!-- date de l'idée --></p>
+                </div>
+            </div>
+    </div>
+</div>
+
+</header>
